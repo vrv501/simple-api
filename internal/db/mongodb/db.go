29d9129c37a376ae138cd1e8691a484e773105bc
vrv501 <33621393+vrv501@users.noreply.github.com>
2025-08-10 15:22:19 +0000

    add mongo db go types

--- internal/db/mongodb/db.go
+++ internal/db/mongodb/db.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readconcern"
@@ -73,4 +74,46 @@ func NewInstance(ctx context.Context) *MongoClient {
 
 func (m *MongoClient) Close(ctx context.Context) error {
 	return m.client.Disconnect(ctx)
+}
+
+type animalCategory struct {
+	ID        bson.ObjectID `bson:"_id,omitempty"`
+	Name      string        `bson:"name"`       // "bsonType": "string"
+	CreatedOn time.Time     `bson:"created_on"` // "bsonType": "date"
+	UpdatedOn *time.Time    `bson:"updated_on"` // "bsonType": ["date", "null"]
+	DeletedOn *time.Time    `bson:"deleted_on"` // "bsonType": ["date", "null"]
+}
+
+type pet struct {
+	ID         bson.ObjectID `bson:"_id,omitempty"`
+	Name       string        `bson:"name"`        // "bsonType": "string"
+	CategoryID bson.ObjectID `bson:"category_id"` // "bsonType": "objectId"
+	PhotoURLs  []string      `bson:"photo_urls"`  // "bsonType": ["array", "null"]
+	Status     string        `bson:"status"`      // "bsonType": "string"
+	Tags       []string      `bson:"tags"`        // "bsonType": ["array", "null"]
+	CreatedOn  time.Time     `bson:"created_on"`  // "bsonType": "date"
+	UpdatedOn  *time.Time    `bson:"updated_on"`  // "bsonType": ["date", "null"]
+	DeletedOn  *time.Time    `bson:"deleted_on"`  // "bsonType": ["date", "null"]
+}
+
+type user struct {
+	ID          bson.ObjectID `bson:"_id,omitempty"`
+	Username    string        `bson:"username"`     // "bsonType": "string"
+	FullName    string        `bson:"full_name"`    // "bsonType": "string"
+	Email       string        `bson:"email"`        // "bsonType": "string"
+	Password    string        `bson:"password"`     // "bsonType": "string"
+	PhoneNumber string        `bson:"phone_number"` // "bsonType": "string"
+	CreatedOn   time.Time     `bson:"created_on"`   // "bsonType": "date"
+	UpdatedOn   *time.Time    `bson:"updated_on"`   // "bsonType": ["date", "null"]
+	DeletedOn   *time.Time    `bson:"deleted_on"`   // "bsonType": ["date", "null"]
+}
+
+type order struct {
+	ID          bson.ObjectID `bson:"_id,omitempty"`
+	UserID      bson.ObjectID `bson:"user_id"`      // "bsonType": "objectId"
+	PetID       bson.ObjectID `bson:"pet_id"`       // "bsonType": "objectId"
+	ShippedDate *time.Time    `bson:"shipped_date"` // "bsonType": ["date", "null"]
+	Status      string        `bson:"status"`       // "bsonType": "string"
+	CreatedOn   time.Time     `bson:"created_on"`   // "bsonType": "date"
+	DeletedOn   *time.Time    `bson:"deleted_on"`   // "bsonType": ["date", "null"]
 }
